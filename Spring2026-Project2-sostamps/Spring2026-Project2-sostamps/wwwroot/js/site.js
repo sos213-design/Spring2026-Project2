4a26528ded3b15329be6715821b2647145d4ab6e
Shelton Stamps <sheltonstamps2@gmail.com>
2026-02-27 13:12:47 -0600

    Added some JS functionallity

--- Spring2026-Project2-sostamps/Spring2026-Project2-sostamps/wwwroot/js/site.js
+++ Spring2026-Project2-sostamps/Spring2026-Project2-sostamps/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function toggleVisibility(id) {
+    const element = document.getElementById(id);
+    if (element.style.visibility === "hidden" || element.style.visibility === "") {
+        element.style.visibility = "visible";
+    }
+        else {
+        element.style.visibility = "hidden";
+    }
+}
+
+function time(){
+    
+}
+
+
